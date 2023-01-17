d2809cb4ee2118ea27b1fc5270e1cf090a6a445d
Daniel Cunningham <danrcunn@vols.utk.edu>
2023-01-17 13:53:19 -0600

    Created the Random Number guessing game.

--- GettingIntoGit/Program.cs
+++ GettingIntoGit/Program.cs
@@ -7,8 +7,26 @@ namespace GettingIntoGit
         
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
-            Console.ReadKey();
+            Random rand = new Random();
+            Console.WriteLine("You're now playing a guessing game. Guess a number between 1 and 100");
+            int randomNumber = rand.Next(1, 100);
+            Console.Write("Guess a number: ");
+            string guess = Console.ReadLine();
+            int guessI32 = Int32.Parse(guess);
+            if(guessI32 < randomNumber)
+            {
+                Console.WriteLine("Your guess was too low.");
+            }
+            else if (guessI32 > randomNumber)
+            {
+                Console.WriteLine("Your guess was too high.");
+            }
+            else if (guessI32 == randomNumber)
+            {
+                Console.WriteLine("Your guess was right!!");
+            }
+            Console.Write("Press enter to end the program.");
+            Console.Read();
         }
     }
 }
